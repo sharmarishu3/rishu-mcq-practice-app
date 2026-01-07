3a184affe5a15b35646204f9014daa961fcae9f0
sharmarishu3 <sharmarishu8385@gmail.com>
2026-01-07 13:44:20 +0530

    Add questions and connect index.html

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+questions = [
+    {
+        "question": "What is the capital of India?",
+        "options": ["Delhi", "Mumbai", "Chennai", "Kolkata"],
+        "answer": "Delhi"
+    },
+    {
+        "question": "ભારતની રાજધાની શું છે?",
+        "options": ["દિલ્લી", "મુંબઇ", "ચેન્નાઈ", "કોલકાતા"],
+        "answer": "દિલ્લી"
+    }
+]
 
 @app.route("/")
 def home():
-    return "MCQ Practice App – Working"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
