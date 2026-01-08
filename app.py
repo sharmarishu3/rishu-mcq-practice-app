afda36df96b435b89432a69bc496ab821d2be67a
sharmarishu3 <sharmarishu8385@gmail.com>
2026-01-08 09:37:46 +0530

    Final MCQ working version

--- app.py
+++ app.py
@@ -5,29 +5,29 @@ app = Flask(__name__)
 
 questions = [
     {
-        "question": "20वीं सदी का सबसे बड़ा युद्ध कौन सा था?",
-        "options": ["प्रथम विश्व युद्ध", "द्वितीय विश्व युद्ध", "शीत युद्ध", "कोई नहीं"],
-        "answer": "द्वितीय विश्व युद्ध"
-    },
-    {
-        "question": "भारत की राजधानी क्या है?",
-        "options": ["दिल्ली", "मुंबई", "चेन्नई", "कोलकाता"],
-        "answer": "दिल्ली"
-    },
-    {
-        "question": "गुजरात की राजधानी क्या है?",
-        "options": ["सूरत", "राजकोट", "गांधीनगर", "अहमदाबाद"],
-        "answer": "गांधीनगर"
-    },
-    {
         "question": "What is the capital of India?",
         "options": ["Delhi", "Mumbai", "Chennai", "Kolkata"],
         "answer": "Delhi"
     },
     {
-        "question": "Which planet is known as Red Planet?",
-        "options": ["Earth", "Mars", "Jupiter", "Venus"],
-        "answer": "Mars"
+        "question": "ભારતની રાજધાની શું છે?",
+        "options": ["દિલ્લી", "મુંબઈ", "ચેન્નઈ", "કોલકાતા"],
+        "answer": "દિલ્લી"
+    },
+    {
+        "question": "Gujarat ni rajdhani shu chhe?",
+        "options": ["Surat", "Rajkot", "Gandhinagar", "Ahmedabad"],
+        "answer": "Gandhinagar"
+    },
+    {
+        "question": "2 + 2 = ?",
+        "options": ["3", "4", "5", "6"],
+        "answer": "4"
+    },
+    {
+        "question": "Sun rises in the?",
+        "options": ["North", "South", "East", "West"],
+        "answer": "East"
     }
 ]
 
