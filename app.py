8fab3191fd16288b4fbff8ce4115aa93f112b734
sharmarishu3 <sharmarishu8385@gmail.com>
2026-01-08 08:17:53 +0530

    Perfect MCQ app with working Next button and score

--- app.py
+++ app.py
@@ -3,10 +3,28 @@ import os
 
 app = Flask(__name__)
 
+questions = [
+    {
+        "question": "20वीं सदी का सबसे बड़ा युद्ध कौन सा था?",
+        "options": ["प्रथम विश्व युद्ध", "द्वितीय विश्व युद्ध", "शीत युद्ध", "कोई नहीं"],
+        "answer": "द्वितीय विश्व युद्ध"
+    },
+    {
+        "question": "भारत की राजधानी क्या है?",
+        "options": ["दिल्ली", "मुंबई", "चेन्नई", "कोलकाता"],
+        "answer": "दिल्ली"
+    },
+    {
+        "question": "गुजरात की राजधानी क्या है?",
+        "options": ["सूरत", "राजकोट", "गांधीनगर", "अहमदाबाद"],
+        "answer": "गांधीनगर"
+    }
+]
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", questions=questions)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
