acb17dbd6f0ce70a2802eccf6bc3e5c5a2a38ef2
sharmarishu3 <sharmarishu8385@gmail.com>
2026-01-08 07:39:13 +0530

    Final working MCQ app

--- app.py
+++ app.py
@@ -2,19 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-questions = [
-    {
-        "question": "20वीं सदी का सबसे बड़ा युद्ध कौन सा था?",
-        "options": ["प्रथम विश्व युद्ध", "द्वितीय विश्व युद्ध", "शीत युद्ध", "कोई नहीं"],
-        "answer": "द्वितीय विश्व युद्ध"
-    },
-    {
-        "question": "भारत की राजधानी क्या है?",
-        "options": ["दिल्ली", "मुंबई", "चेन्नई", "कोलकाता"],
-        "answer": "दिल्ली"
-    }
-]
-
 @app.route("/")
 def home():
     return render_template("index.html")
